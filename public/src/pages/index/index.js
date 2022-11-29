d326886b1b4cc7953a78d6fbf01c99683a8cdbdd
Tony <white87332@gmail.com>
2022-11-29 11:47:42 +0800

    add react.memo, usememo

--- public/src/pages/index/index.js
+++ public/src/pages/index/index.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import Child from './child';
+import ChildMemo from './childMemo';
 import styles from './index.module.css';
 
 export default function Index(props)
 {
+    const [step, setStep] = useState(0);
+    const [count, setCount] = useState(0);
+    const [number, setNumber] = useState(0);
+
+    const memoizedValue = useMemo(() => {
+        return Math.random();
+    }, []);
+
+    console.log('memoizedValue', memoizedValue);
+
     return (
         <div className={styles.index}>
-            <div className={styles.test}>
-                test
-            </div>
+            <button type="button" onClick={() => setStep(step + 1)}>
+                step is :
+                {step}
+            </button>
 
-            <div className={styles.aaa}>
-                aaa
-            </div>
+            <button type="button" onClick={() => setCount(count + 1)}>
+                count is :
+                {count}
+            </button>
+
+            <button type="button" onClick={() => setNumber(count + step)}>
+                number is :
+                {number}
+            </button>
+
+            <hr />
+
+            <Child step={step} count={count} number={number} />
+            <hr />
+
+            <ChildMemo step={step} count={count} number={number} />
         </div>
     );
 }
